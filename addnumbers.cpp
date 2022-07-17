5bf4136c03f59a0fc5679f2aff2e3a0d907d73e6
Pratik Sinha <pratik1971sinha@gmail.com>
2022-07-17 10:25:35 +0530

    CPP and DSA

---
+++ addnumbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 10;
+  int b = 15;
+  int c = a + b;
+
+  cout << c << endl;
+
+  char d = 'x';
+  cout << d << endl;
+
+  float f = 1.23;
+  cout << f << endl;
+
+  bool g = true;
+  cout << g << endl;
+
+  int size = sizeof(f);
+  cout << size << endl;
+
+}
